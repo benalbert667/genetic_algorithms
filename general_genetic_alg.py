accda4de70e299e949b6b6f2eaf9bcfccfc3c0c4
Ben Albert <benalbert667@gmail.com>
2023-10-03 16:01:27 -0400

    define GAHeap

--- general_genetic_alg.py
+++ general_genetic_alg.py
@@ -60,3 +60,77 @@ class GGA:
     def __get_elite_of_population(self):
         # Get the top (1-br)% of population by fitness
         return np.array(sorted(self.__population, key=self.check_fitness))[-int(self.ps*(1-self.br)):]
+
+
+"""
+Heap data structure used to efficiently get the elite of a population
+"""
+
+
+def parent(i):
+    return (i - 1) // 2
+
+
+def l_child(i):
+    return i * 2 + 1
+
+
+def r_child(i):
+    return i * 2 + 2
+
+
+class GAHeap:
+    def __init__(self, size, ranker):
+        self.len = size
+        self.heap = [None] * size
+        self.nextRB = 0
+        self.rank = ranker
+
+    def insert(self, g):
+        self.heap[self.nextRB] = (g, self.rank(g))
+        self.perc_up()
+        self.nextRB += 1
+
+    def delete_max(self):
+        self.nextRB -= 1
+        self.heap[0], self.heap[self.nextRB] = self.heap[self.nextRB], self.heap[0]
+
+        to_return = self.heap[self.nextRB]
+        self.heap[self.nextRB] = None
+
+        self.perc_down()
+
+        return to_return
+
+    def perc_up(self):
+        i = self.nextRB
+        while i > 0 and self.heap[i][1] > self.heap[parent(i)][1]:
+            self.heap[i], self.heap[parent(i)] = self.heap[parent(i)], self.heap[i]
+            i = parent(i)
+
+    def perc_down(self):
+        i = 0
+        while not self.is_leaf(i) and self.is_less_than_child(i):
+            if self.heap[l_child(i)] is None:
+                j = r_child(i)
+            elif self.heap[r_child(i)] is None:
+                j = l_child(i)
+            elif self.heap[l_child(i)][1] > self.heap[r_child(i)][1]:
+                j = l_child(i)
+            else:
+                j = r_child(i)
+
+            self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+            i = j
+
+    def is_leaf(self, i):
+        if l_child(i) >= self.len or r_child(i) >= self.len:
+            return True
+        return self.heap[l_child(i)] is None and self.heap[r_child(i)] is None
+
+    def is_less_than_child(self, i):
+        if self.heap[l_child(i)] is None and self.heap[r_child(i)] is not None:
+            return self.heap[r_child(i)][1] > self.heap[i][1]
+        elif self.heap[l_child(i)] is not None and self.heap[r_child(i)] is None:
+            return self.heap[l_child(i)][1] > self.heap[i][1]
+        return self.heap[l_child(i)][1] > self.heap[i][1] or self.heap[r_child(i)][1] > self.heap[i][1]
