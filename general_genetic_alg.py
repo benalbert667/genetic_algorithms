96d8ba7759fee2fd63d8b0ea04c762283581eba9
Ben Albert <benalbert667@gmail.com>
2023-10-05 15:06:24 -0400

    implemented heap usage to GGA

--- general_genetic_alg.py
+++ general_genetic_alg.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.random import rand, shuffle
+from random import random
 from math import ceil
 
 
@@ -8,58 +9,82 @@ class GGA:
         self.mr = mutate_rate  # chance for a gene to mutate
         self.br = breed_rate  # percent of population that is cycled per generation
         self.ps = population_size  # size of population
+        self.os = len_output  # size of each individual
         self.check_fitness = success_function  # fitness function (higher result = more fit)
 
-        self.__population = rand(self.ps, len_output)  # entire population
-        self.__best_individuals = None  # sorted list of top (1-br)% individuals
+        self.__population = GAHeap(self.ps, self.check_fitness)  # entire population
+        self.__init_population()
+        # self.__best_individuals = None  # sorted list of top (1-br)% individuals
         self.__generation = 0
 
     def get_best_individual(self):
         # Returns the individual from the current population with the highest fitness
-        if self.__best_individuals is not None:
-            return self.__best_individuals[-1].copy(), self.check_fitness(self.__best_individuals[-1])
-        return None
+        return self.__population.get_max()
 
     def increment_generation(self, num_generations):
-        if self.__generation == 0:
-            self.__best_individuals = self.__get_elite_of_population()
+        # if self.__generation == 0:
+        #     self.__best_individuals = self.__get_elite_of_population()
         for _ in range(num_generations):
             self.__breed_random_parents()
             self.__generation += 1
-            self.__best_individuals = self.__get_elite_of_population()
-
-    def get_current_population(self):
-        return self.__population.copy()
+            # self.__best_individuals = self.__get_elite_of_population()
 
     def get_num_generations(self):
         return self.__generation
 
     def __breed_random_parents(self):
-        ga = rand(*self.__population.shape)  # Gene assignments
-        # Create parent pool from current elite of population
-        parents = np.repeat(self.__best_individuals, int(ceil((1 / (1 - self.br)))), axis=0)
-        shuffle(parents)
-        # Resize parents to population's size (cutting off a random selection of repeated parents)
-        parents = parents[:self.__population.shape[0]]
+        num_elites = int(self.ps * (1 - self.br))
+        elites = [self.__population.delete_max() for _ in range(num_elites)]
 
-        # Create mother and father populations
-        mothers = parents.copy()
-        shuffle(parents)
-        fathers = parents.copy()
+        # proportional ranges based on score from 1 to self.mr
+        ess = sum(e[1] for e in elites)  # elite score sum
+        ppoi = np.cumsum([(e[1] / ess) * (1 - self.mr) for e in elites])  # proportional probabilities of inheritance
 
-        # Parent Choice Threshold: Used to determine which parent a child should inherit a gene from
-        pct = (1 - self.mr) / 2 + self.mr
-        # Wipe current population
-        self.__population = rand(*self.__population.shape)
-        # Inherit genes randomly from either the father or the mother gene pools (or randomly mutate)
-        self.__population = np.where((ga > self.mr) & (ga <= pct), fathers,
-                                     np.where(ga > pct, mothers, self.__population))
-        # Re-introduce previous generation's elite into population
-        self.__population[:self.__best_individuals.shape[0]] = self.__best_individuals
+        self.__population.clear_heap()
 
-    def __get_elite_of_population(self):
-        # Get the top (1-br)% of population by fitness
-        return np.array(sorted(self.__population, key=self.check_fitness))[-int(self.ps*(1-self.br)):]
+        for _ in range(self.ps - num_elites):
+            ga = rand(self.os)
+            indv = rand(self.os)
+            for i in range(self.os):
+                j = np.searchsorted(ppoi, ga[i])
+                if j < num_elites:
+                    indv[i] = elites[j][0][i]
+            self.__population.insert(indv)
+
+        while elites:
+            self.__population.insert(elites.pop()[0])
+
+
+
+        # ga = rand(*self.__population.shape)  # Gene assignments
+        # # Create parent pool from current elite of population
+        # parents = np.repeat(self.__best_individuals, int(ceil((1 / (1 - self.br)))), axis=0)
+        # shuffle(parents)
+        # # Resize parents to population's size (cutting off a random selection of repeated parents)
+        # parents = parents[:self.__population.shape[0]]
+        #
+        # # Create mother and father populations
+        # mothers = parents.copy()
+        # shuffle(parents)
+        # fathers = parents.copy()
+        #
+        # # Parent Choice Threshold: Used to determine which parent a child should inherit a gene from
+        # pct = (1 - self.mr) / 2 + self.mr
+        # # Wipe current population
+        # self.__population = rand(*self.__population.shape)
+        # # Inherit genes randomly from either the father or the mother gene pools (or randomly mutate)
+        # self.__population = np.where((ga > self.mr) & (ga <= pct), fathers,
+        #                              np.where(ga > pct, mothers, self.__population))
+        # # Re-introduce previous generation's elite into population
+        # self.__population[:self.__best_individuals.shape[0]] = self.__best_individuals
+
+    # def __get_elite_of_population(self):
+    #     # Get the top (1-br)% of population by fitness
+    #     return np.array(sorted(self.__population, key=self.check_fitness))[-int(self.ps*(1-self.br)):]
+
+    def __init_population(self):
+        for _ in range(self.ps):
+            self.__population.insert(rand(self.os))
 
 
 """
@@ -82,7 +107,7 @@ def r_child(i):
 class GAHeap:
     def __init__(self, size, ranker):
         self.len = size
-        self.heap = [None] * size
+        self.heap = [None] * self.len
         self.nextRB = 0
         self.rank = ranker
 
@@ -101,6 +126,13 @@ class GAHeap:
         self.perc_down()
 
         return to_return
+
+    def get_max(self):
+        return self.heap[0]
+
+    def clear_heap(self):
+        self.heap = [None] * self.len
+        self.nextRB = 0
 
     def perc_up(self):
         i = self.nextRB
